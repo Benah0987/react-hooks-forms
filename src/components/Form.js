9b7e967eae8b20d49846c90d0759dd4b147723ee
Benah0987 <wanyoikebenayah77@gmail.com>
2023-04-22 21:39:08 +0300

    added the handleLastName function

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,11 +3,22 @@ import React, { useState } from "react";
 function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
+  
+  //function firstName
+  //passed an event asa parameter
+  function handleFirstNameChange(event) {
+    setFirstName(event.target.value);
+    
+  }
+  //handles the last name
+  function handleLastNameChange(event) {
+    setLastName(event.target.value)
+  }
 
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" onChange ={handleFirstNameChange} value={firstName} />
+      <input type="text" onChange ={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
   );
